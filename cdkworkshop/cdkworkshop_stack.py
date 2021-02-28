217f82da8260bd87aea12bcd046915c1595f43f2
Martin Trejo <chillicoder@gmail.com>
2021-02-28 15:18:36 -0600

    Adds TableViewer CDK construct

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     core
 )
 
+from cdk_dynamo_table_viewer import TableViewer
 from hitcounter import HitCounter
 
 class CdkworkshopStack(core.Stack):
@@ -26,4 +27,11 @@ class CdkworkshopStack(core.Stack):
         apigw.LambdaRestApi(
             self, 'Endpoint',
             handler=hello_with_counter.handler,
+        )
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+            sort_by='-hits'
         )
--- cdkworkshop/hitcounter.py
+++ cdkworkshop/hitcounter.py
@@ -10,10 +10,14 @@ class HitCounter(core.Construct):
     def handler(self):
         return self._handler
 
+    @property
+    def table(self):
+        return self._table
+
     def __init__(self, scope: core.Construct, id: str, downstream: _lambda.IFunction, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        table = ddb.Table(
+        self._table = ddb.Table(
             self, 'Hits',
             partition_key={'name': 'path', 'type': ddb.AttributeType.STRING}
         )
@@ -25,9 +29,9 @@ class HitCounter(core.Construct):
             code=_lambda.Code.asset('lambda'),
             environment={
                 'DOWNSTREAM_FUNCTION_NAME': downstream.function_name,
-                'HITS_TABLE_NAME': table.table_name
+                'HITS_TABLE_NAME': self._table.table_name
             }
         )
 
-        table.grant_read_write_data(self.handler)
+        self._table.grant_read_write_data(self.handler)
         downstream.grant_invoke(self.handler)
